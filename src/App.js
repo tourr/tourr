647933365f628b9e24f3ec27ef0641ef1145daba
tourr <36723917+tourr@users.noreply.github.com>
2018-02-22 17:26:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          HELLO WORLD !
-        </p>
       </div>
     );
   }
